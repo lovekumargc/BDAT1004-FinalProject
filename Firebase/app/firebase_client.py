1d85de6a874ae15a2528541bd6e9d9f3531e52fe
lovekumargc <66360571+lovekumargc@users.noreply.github.com>
2020-08-08 01:26:37 +0530

    Update firebase_client.py

--- Firebase/app/firebase_client.py
+++ Firebase/app/firebase_client.py
@@ -4,14 +4,14 @@ from datetime import datetime
 
 class FirebaseClient:
     firebaseConfig = {
-        "apiKey": "",
-        "authDomain": "",
-        "databaseURL": "",
-        "storageBucket": "",
-        "projectId": "",
-        "messagingSenderId": "",
-        "appId": "",
-        "measurementId": ""
+        "apiKey": "AIzaSyCZdJSflZ-287I7fiL1jh4YJ3BbpPx3IGU",
+        "authDomain": "bdat-web-app.firebaseapp.com",
+        "databaseURL": "https://bdat-web-app.firebaseio.com",
+        "storageBucket": "bdat-web-app.appspot.com",
+        "projectId": "bdat-web-app",
+        "messagingSenderId": "466614626356",
+        "appId": "1:466614626356:web:5ab21f73bad8afa0538261",
+        "measurementId": "G-Q91NYNGXJR"
     }
 
     def __init__(self):
